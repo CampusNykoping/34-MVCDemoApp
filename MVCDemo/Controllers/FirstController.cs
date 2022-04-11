f6c0477aceaf308c3ac51db6a2806d6972b5bde3
Claes Engelin <claes.engelin@zocom.se>
2022-04-11 13:57:14 +0200

    Lagt till en controller med tre olika actions, en modell samt två vyer.

---
+++ MVCDemo/Controllers/FirstController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+using MVCDemo.Models;
+
+namespace MVCDemo.Controllers
+{
+    public class FirstController : Controller
+    {
+        public string Index()
+        {
+            return "Hello, World!";
+        }
+
+        public IActionResult Hello()
+        {
+            ViewBag.Message = "Hej på dig, hur är läget?";
+            return View();
+        }
+
+        public IActionResult Info()
+        {
+            var person = new Person();
+            person.Name = "Claes";
+            person.Age = 65;
+            person.Location = "Göteborg";
+
+            return View(person);
+        }
+    }
+}
---
+++ MVCDemo/Models/Person.cs
@@ -0,0 +1,8 @@
+﻿namespace MVCDemo.Models;
+
+public class Person
+{
+    public string Name { get; set; } = String.Empty;
+    public int Age { get; set; }
+    public string Location { get; set; } =  String.Empty;
+}
